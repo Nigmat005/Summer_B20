6d9017e58f5a8719e8be4190e58e7b13d0a09c4e
Nigmat005 <Nigmathalmamat@gmail.com>
2020-08-03 21:47:21 -0400

    Regular way

---
+++ src/replt/Method_with_String_extractNumberfromString.java
@@ -0,0 +1,18 @@
+package replt;
+
+public class Method_with_String_extractNumberfromString {
+
+    public static void main(String[] args) {
+        String end=extractNum("AAb257");
+        System.out.println(end);
+        System.out.println(extractNum("aa2aa3"));
+    } public static String extractNum(String s) {
+        String result="";
+        for(char each: s.toCharArray()){
+            if(Character.isDigit(each)){
+                result+=each;
+            }
+        }return result;
+
+    }
+}
